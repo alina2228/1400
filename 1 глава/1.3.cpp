8a617d492488594eec34ca34284f418e66601b6c
alina2228 <lev2106@icloud.com>
2025-11-01 01:28:54 +0300

    Add files via upload

---
+++ 1 глава/1.3.cpp
@@ -0,0 +1,10 @@
+﻿/* 1.3.	 Вывести на экран числа 50 и  10 одно под другим */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << 50 << std::endl << 10 << std::endl;
+	std::system("pause");
+	return 0;	
+}
---
+++ 1 глава/1.4.cpp
@@ -0,0 +1,10 @@
+﻿/* Вывести на экран числа 5, 10 и  21 одно под другим. */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "5\n10\n21\n";
+	std::system("pause");
+	return 0;
+}
